9f78ff9998ce6a6248cf58a31d333434b89c428f
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-08-22 17:21:07 +0530

    Secured http

--- src/modules/Product/index.js
+++ src/modules/Product/index.js
@@ -8,7 +8,7 @@ function Product() {
     const [product, setProduct] = useState({})
     useEffect(() => {
         const fetchProduct = async () => {
-            const response = await fetch(`http://fakestoreapi.com/products/${id}`);
+            const response = await fetch(`https://fakestoreapi.com/products/${id}`);
             const data = await response.json();
             
             // console.log(data);
